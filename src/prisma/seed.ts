303c2eaa95cd22a37c697dead99d72e202e024bb
Xujaniyozov <akrom2661@gmail.com>
2026-04-19 21:13:18 +0500

    writing contants datas

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -52,13 +52,3 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
